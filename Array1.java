d1a2dac3b52695f3cf3ac39bcbbe3ce433e2cb16
janecres227 <janecresd@gmail.com>
2025-02-21 19:37:01 +0800

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,24 @@
+public class Array1 {
+    public static void main(String[] args) {
+        String[] friends = new String[5];
+        //Names
+        friends[0] = "Dahulo";
+        friends[1] = "Tayson";
+        friends[2] = "Darudoh";
+        friends[3] = "Bakalkal";
+        friends[4] = "Boknoy";
+        //Age
+        int[] age = new int[6];
+        age[0] = 23;
+        age[1] = 21;
+        age[2] = 29;
+        age[3] = 27;
+        age[4] = 28;
+
+        System.out.println(friends[1]);
+        System.out.println(age[3]);
+
+
+    }
+    
+}
