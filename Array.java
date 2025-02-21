7d194c48a68de1353faf8e8c5d56b0f76c5bcc22
janecres227 <janecresd@gmail.com>
2025-02-21 19:35:35 +0800

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] names = {"Jan", "Goryo", "Dahulo", "Simeon"};
+        int[] numbers = {23,25,29,30};
+
+        System.out.println(names[1]);
+        System.out.println(numbers[3]);
+    }
+    
+}
