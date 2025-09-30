9bce65cd7eee5741eb5ae6a8a3e0f1f50dcd0b9d
janecres227 <janecresd@gmail.com>
2025-09-30 22:38:45 +0800

    Gelo, 49

--- Array.java
+++ Array.java
@@ -1,7 +1,7 @@
 public class Array {
     public static void main(String[] args) {
-        String[] names = {"Jan", "Goryo", "Dahulo", "Simeon"};
-        int[] numbers = {23,25,29,30};
+        String[] names = {"Jan", "Goryo", "Dahulo", "Simeon", "Gelo"};
+        int[] numbers = {23,25,29,30,49};
 
         System.out.println(names[1]);
         System.out.println(numbers[3]);
